6007ff88d482ef4513237a112243df210693a68b
Conrad Kramer <Conradakramer@gmail.com>
2020-04-06 08:36:19 -0500

    push for portfolio

--- rstracker/rstracker.js
+++ rstracker/rstracker.js
@@ -61,6 +61,63 @@ function aboutPerson(){
         listElements.appendChild(displayResults(cleanList[i]));
     }
 };
+function displayPotions(){
+    const itemList = JSON.parse(localStorage.getItem('items'));
+    const cleanList = [];
+     for(x in itemList)
+     {
+         cleanList.push(itemList[x]);    
+     }
+     console.log(cleanList);
+     for(x in cleanList){
+         if (cleanList[x].sell_average == 0)
+         {
+             cleanList[x].diffrence = 0;
+         }
+         else if(cleanList[x].buy_average == 0)
+         {
+             cleanList[x].diffrence = 0;
+         }
+         else{
+             cleanList[x].diffrence = cleanList[x].sell_average - cleanList[x].buy_average;
+         } 
+     }
+     cleanList.sort(compareValues('diffrence', 'desc'));
+     for(i = 0; i < 2500 ; i++){
+         if(cleanList[i].name.includes('potion')){
+            listElements.appendChild(displayResults(cleanList[i]));
+         }
+     }
+ };
+
+ function displaySets(){
+    const itemList = JSON.parse(localStorage.getItem('items'));
+    const cleanList = [];
+     for(x in itemList)
+     {
+         cleanList.push(itemList[x]);    
+     }
+     console.log(cleanList);
+     for(x in cleanList){
+         if (cleanList[x].sell_average == 0)
+         {
+             cleanList[x].diffrence = 0;
+         }
+         else if(cleanList[x].buy_average == 0)
+         {
+             cleanList[x].diffrence = 0;
+         }
+         else{
+             cleanList[x].diffrence = cleanList[x].sell_average - cleanList[x].buy_average;
+         } 
+     }
+     cleanList.sort(compareValues('diffrence', 'desc'));
+     for(i = 0; i < 2500 ; i++){
+         if(cleanList[i].name.includes('set')){
+            listElements.appendChild(displayResults(cleanList[i]));
+         }
+     }
+ };
 
 function displayResults(item) {
     //console.log(item);
@@ -88,7 +145,28 @@ function refreshPage(){
 
     listElements.innerHTML = '';
     displayItems();
-}
+};
+function potions(){
+    $.getJSON('https://api.allorigins.win/get?url=' + encodeURIComponent('https://rsbuddy.com/exchange/summary.json'), function (data) {
+                          
+        const parsed = JSON.parse(data.contents);
+        const newJson = JSON.stringify(parsed);
+        localStorage.setItem('items', newJson );
+    });
+    listElements.innerHTML = '';
+    displayPotions();
+};
+
+function sets(){
+    $.getJSON('https://api.allorigins.win/get?url=' + encodeURIComponent('https://rsbuddy.com/exchange/summary.json'), function (data) {
+                          
+        const parsed = JSON.parse(data.contents);
+        const newJson = JSON.stringify(parsed);
+        localStorage.setItem('items', newJson );
+    });
+    listElements.innerHTML = '';
+    displaySets();
+};
 
 refreshPage();
 
